aa16450dd25c618232314440cc3f72141dd7e703
KhoaHoangMinh <hoangkhoa2015@gmail.com>
2025-02-20 20:02:13 +0700

    Update Paladin

--- source/Paladin.java
+++ source/Paladin.java
@@ -5,6 +5,13 @@ public class Paladin extends Knight {
 
 	@Override
 	public double getCombatScore() {
-		return 1.0;
+		double combatScore;
+		boolean condition = Utility.isFibo(getBaseHp()) && Utility.findFiboIndex(getBaseHp()) > 2;
+		if(condition) {
+			combatScore = 1000 + Utility.findFiboIndex(getBaseHp());
+		} else {
+			combatScore = getBaseHp() * 3;
+		}
+		return combatScore;
 	}
 }
